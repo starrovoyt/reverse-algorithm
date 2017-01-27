15081f6aa16931c0ec8a677c3342b9f3764d4e8c
Ann Starovoyt <starrovoyt@gmail.com>
2017-01-27 23:40:44 +0300

    Initial Commit

---
+++ Reverse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Reverse
+//
+//  Created by Анна Старовойт on 27.01.17.
+//  Copyright © 2017 Ann Starovoyt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
